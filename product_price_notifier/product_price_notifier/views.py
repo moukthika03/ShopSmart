ce88a17d33c224c8c26eb3821690b1e588986fc8
moukthika03 <moukthika@gmail.com>
2023-03-02 13:45:31 +0530

    Extracted price list

--- product_price_notifier/product_price_notifier/views.py
+++ product_price_notifier/product_price_notifier/views.py
@@ -9,11 +9,20 @@ shopclues_url = "https://www.shopclues.com/search?q="
 def web_scrape(url, product_name):
     webpage = requests.get(url+product_name)
     soup = BeautifulSoup(webpage.content, "html.parser")
+    # print(soup.prettify())
     text = soup.get_text().strip()
-    print(text)
+    prices = soup.find_all("div", class_= '_30jeq3')
+    refined_prices=[]
+    for price in prices:
+        price = price.get_text() # Will get text from html tags
+        price = price[1:]
+        price = price.replace(",","")
+        refined_prices.append(float(price.strip())) # Removing special characters like \n (newline)
+    print(refined_prices)
 
 def home(request):
-    product_name=request.POST.get("product_name")
-    web_scrape(flipkart_url, product_name)
-    web_scrape(shopclues_url, product_name)
+    if request.method == "POST":
+        product_name=request.POST.get("product_name")
+        web_scrape(flipkart_url, product_name)
+        
     return render(request,"home.html")
